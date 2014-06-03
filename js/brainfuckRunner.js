9bd6c42cc21ef9915bf8241d0b5a3a9c1d1080e0
Niels Schroyen <nielsschroye@gmail.com>
2014-06-03 21:02:06 +0200

    show and hide messages

--- js/brainfuckRunner.js
+++ js/brainfuckRunner.js
@@ -3,12 +3,34 @@ function runBrainfuck()
   var instructions = document.getElementById("instructions").value;
   var input = document.getElementById("input").value;
 
-  
+  if(instructions === '')
+  {
+   showError('please enter some instructions');
+  }
+  else
+  {
   var output = brainfuck(instructions,input);
-  
-  
-  var outputP = document.getElementById("output");
-  
-  
+  showOutput(output);
+  }
+   
+   
+}
+
+
+function showOutput(output)
+{
+  var outputP = document.getElementById("output"); 
+  var errorP = document.getElementById("error"); 
   outputP.innerHTML = 'Output: <br/>'+output;
+  outputP.style.display = '';
+  errorP.style.display = 'none';
+}
+
+function showError(error)
+{
+  var outputP = document.getElementById("output"); 
+  var errorP = document.getElementById("error"); 
+  errorP.innerHTML = 'Something went wrong: <br/>'+error;
+  outputP.style.display = 'none';
+  errorP.style.display = '';
 }
